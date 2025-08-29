c2b9ae7eb0000483c46abfbcdc5494fd05b0a9b9
mehdi4430 <mehdi.target0@gmail.com>
2025-08-29 11:38:07 +0330

    Update g.py

--- g.py
+++ g.py
@@ -70,18 +70,32 @@ def paziresh24(phone):
         formatted_phone = f"0{formatted_phone}"
         
         headers = {
-            "Accept": "application/json, text/plain, */*",
-            "accept-timezone": "Asia/Tehran",
-            "content-type": "application/json; charset=utf-8",
-            "User-Agent": random.choice(user_agents),
+            "Authorization": "Splunk cd46b97e-bf0d-46e4-ba7e-111c2f88291f",
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1"
         }
         
         payload = {
-            "mobile": formatted_phone
+            "sourcetype": "_json",
+            "event": {
+                "event_group": "legacy-login-steps",
+                "event_type": "submit-mobile-number",
+                "url": {
+                    "href": "https://www.paziresh24.com/patient/",
+                    "query": "",
+                    "pathname": "/patient/",
+                    "host": "www.paziresh24.com"
+                },
+                "popupForm": True,
+                "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1",
+                "terminal_id": "clinic-68b0f1d69b0897.11243898",
+                "is_application": False,
+                "phone_number": formatted_phone  # اضافه کردن شماره تلفن
+            }
         }
         
         response = requests.post(
-            "https://apigw.paziresh24.com/gozargah/resetpassword",
+            "https://gozargah-splunk.paziresh24.com/services/collector",
             json=payload,
             headers=headers,
             timeout=10,
@@ -89,10 +103,9 @@ def paziresh24(phone):
         )
         
         print(f'{y}[Debug] paziresh24 Status: {response.status_code}{a}')
-        print(f'{y}[Debug] paziresh24 Response: {response.text}{a}')
         
         if response.status_code == 200:
-            print(f'{g}(paziresh24) reset password request sent!{a}')
+            print(f'{g}(paziresh24) log event sent successfully!{a}')
             return True
         else:
             print(f'{r}[-] paziresh24 error: {response.status_code}{a}')
